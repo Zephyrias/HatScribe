c14a484a452b69476bbf162e6ca62a79dcd77ac3
UltraPoulet <johnrabi@umich.edu>
2014-04-16 21:36:28 -0400

    Gambling somewhat works
    Basic gambling gives some rewards now.

--- js/breakgame.js
+++ js/breakgame.js
@@ -19,12 +19,36 @@ function gamble(){
 	// Decrements stars and disables the appropriate buttons
 	// when you run out of stars
   numStars = numStars - 1;
+
+  var hatPer = 25;
+  var expPer = 25;
+  var starPer = 25;
+  var losePer = 25;
+  var result = Math.floor(Math.random()*100);
+  if(result < hatPer){
+    //Drop a hat
+    addNotification("Hat",0);
+  }
+  else if(result < hatPer + expPer){
+    //Give Exp
+    //Maybe a proper amount of EXP as well
+    addNotification("XP",curLevel*100);
+    addExp(curLevel*100);
+  }
+  else if(result < hatPer + expPer + starPer){
+    //Give a star
+    numStars = numStars + 1;
+    //Maybe a notification?
+  }
+  else{
+    //Nothing happens!
+    //WOO!
+  }
   var tempArray = document.getElementsByName('numStars');
   for(var i = 0; i < tempArray.length;i++)
   {
     tempArray[i].innerHTML = numStars;
   }
-  alert('You just gambled!');
 
   if (numStars <= 0)
   {
